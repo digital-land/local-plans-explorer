538043d38c33ed748b83b591b34f6556a3a23f5c
Adam Shimali <adam.shimali@gmail.com>
2024-10-08 11:45:08 +0100

    add filtering to org/plan list

--- application/blueprints/organisation/views.py
+++ application/blueprints/organisation/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, abort, redirect, render_template, request, url_for
 
-from application.models import Organisation
+from application.models import LocalPlan, Organisation, Status
 
 organisation = Blueprint("organisation", __name__, url_prefix="/organisation")
 
@@ -13,12 +13,28 @@ def organisations():
             url_for("organisation.organisation", reference=f"local-authority-eng:{lpa}")
         )
 
-    orgs = (
-        Organisation.query.filter(Organisation.end_date.is_(None))
-        .order_by(Organisation.name)
-        .all()
+    plan_status_filter = request.args.get("planStatusFilter", None)
+    if plan_status_filter and plan_status_filter != "all":
+        status = Status[plan_status_filter]
+        orgs = (
+            Organisation.query.filter(Organisation.end_date.is_(None))
+            .filter(Organisation.local_plans.any(LocalPlan.status == status))
+            .order_by(Organisation.name)
+            .all()
+        )
+    else:
+        orgs = (
+            Organisation.query.filter(Organisation.end_date.is_(None))
+            .order_by(Organisation.name)
+            .all()
+        )
+
+    return render_template(
+        "organisation/index.html",
+        organisations=orgs,
+        statuses=Status,
+        plan_status_filter=request.args.get("planStatusFilter"),
     )
-    return render_template("organisation/index.html", organisations=orgs)
 
 
 @organisation.route("/<string:reference>")
