e82de82242e8590a3229af50acfe27a48e74aaea
Adam Shimali <adam.shimali@gmail.com>
2024-11-25 20:58:14 +0000

    update auth config

--- application/config.py
+++ application/config.py
@@ -2,6 +2,17 @@
 import os
 
 
+def _to_boolean(value):
+    if value is None:
+        return False
+    if isinstance(value, bool):
+        return value
+    if isinstance(value, int):
+        return bool(value)
+    truthy_values = {"1", "t", "true", "y", "yes", "on"}
+    return str(value).lower().strip() in truthy_values
+
+
 class Config(object):
     APP_ROOT = os.path.abspath(os.path.dirname(__file__))
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_ROOT, os.pardir))
@@ -14,7 +25,7 @@ class Config(object):
     SQLALCHEMY_RECORD_QUERIES = False
     DEBUG = False
     WTF_CSRF_ENABLED = True
-    AUTHENTICATION_ON = False
+    AUTHENTICATION_ON = _to_boolean(os.getenv("AUTHENTICATION_ON", True))
     GITHUB_CLIENT_ID = os.getenv("GITHUB_CLIENT_ID")
     GITHUB_CLIENT_SECRET = os.getenv("GITHUB_CLIENT_SECRET")
     SAFE_URLS = set(os.getenv("SAFE_URLS", "").split(","))
@@ -25,7 +36,7 @@ class Config(object):
 class DevelopmentConfig(Config):
     DEBUG = True
     WTF_CSRF_ENABLED = False
-    AUTHENTICATION_ON = True
+    AUTHENTICATION_ON = False
     SQLALCHEMY_RECORD_QUERIES = True
 
 
